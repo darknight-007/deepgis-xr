490d4914a39fdd69c39649ede8934559ff349dc6
darknight-007 <jnaneshwar.das@gmail.com>
2025-01-09 00:00:24 -0700

    tileserver upgrades contd...

--- staticfiles/deepgis/scripts/webclient/raster_layers.js
+++ staticfiles/deepgis/scripts/webclient/raster_layers.js
@@ -4,45 +4,45 @@ const rasterLayers = {
         name: 'BF August 2020 Raster',
         url: 'https://mbtiles.deepgis.org/data/bf_aug_2020_raster/{z}/{x}/{y}.png',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     },
     'bf_oct_2020_raster': {
         name: 'BF October 2020 Raster',
         url: 'https://mbtiles.deepgis.org/data/bf_oct_2020_raster/{z}/{x}/{y}.png',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     },
     'bf_feb_2021_3d': {
         name: 'BF February 2021 3D',
         url: 'https://mbtiles.deepgis.org/data/bf_feb_2021_3d/{z}/{x}/{y}.png',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     },
     'bf_feb_2021_3d_43': {
         name: 'BF February 2021 3D (43)',
         url: 'https://mbtiles.deepgis.org/data/bf_feb_2021_3d_43/{z}/{x}/{y}.png',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     }
 };
 
@@ -52,67 +52,67 @@ const vectorLayers = {
         name: 'BF August 2020 Vector',
         url: 'https://mbtiles.deepgis.org/data/bf_aug_2020_vector/{z}/{x}/{y}.pbf',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     },
     'bf_oct_2020_vector': {
         name: 'BF October 2020 Vector',
         url: 'https://mbtiles.deepgis.org/data/bf_oct_2020_vector/{z}/{x}/{y}.pbf',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     },
     'bf_nov_2020': {
         name: 'BF November 2020',
         url: 'https://mbtiles.deepgis.org/data/bf_nov_2020/{z}/{x}/{y}.pbf',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     },
     'bf_dec_2020': {
         name: 'BF December 2020',
         url: 'https://mbtiles.deepgis.org/data/bf_dec_2020/{z}/{x}/{y}.pbf',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     },
     'bf_dec_2020_alt': {
         name: 'BF December 2020 (Alt)',
         url: 'https://mbtiles.deepgis.org/data/bf_dec_2020_alt/{z}/{x}/{y}.pbf',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     },
     'bf_dec_2020_vector': {
         name: 'BF December 2020 Vector',
         url: 'https://mbtiles.deepgis.org/data/bf_dec_2020_vector/{z}/{x}/{y}.pbf',
         minZoom: 12,
-        maxZoom: 22,
+        maxZoom: 24,
         bounds: [
             [33.78160405323126, -111.2660005204955],
             [33.782606629396106, -111.26454488180822]
         ],
-        center: [33.78210534131368, -111.26527270115186, 17]
+        center: [33.78210534131368, -111.26527270115186, 23]
     }
 };
 
@@ -120,7 +120,7 @@ function initRasterLayers(map) {
     // Create the base layers
     const baseLayers = {
         'Google Satellite': L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-            maxZoom: 22,
+            maxZoom: 24,
             subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
             attribution: '© Google'
         })
@@ -137,6 +137,12 @@ function initRasterLayers(map) {
             tms: false,  // Using XYZ format
             opacity: 0.7,  // Make overlays slightly transparent
             attribution: '© DeepGIS'
+        });
+        
+        // Add event listener for when layer is added to map
+        layer.on('add', () => {
+            // Set view to layer's center point with zoom level 23
+            map.setView([config.center[0], config.center[1]], 23);
         });
         
         overlayLayers[config.name] = layer;
@@ -162,12 +168,18 @@ function initRasterLayers(map) {
             attribution: '© DeepGIS'
         });
         
+        // Add event listener for when layer is added to map
+        layer.on('add', () => {
+            // Set view to layer's center point with zoom level 23
+            map.setView([config.center[0], config.center[1]], 23);
+        });
+        
         overlayLayers[config.name] = layer;
     });
     
     // Set initial view to the center point of the first layer
     const firstLayer = Object.values(rasterLayers)[0];
-    map.setView([firstLayer.center[0], firstLayer.center[1]], firstLayer.center[2]);
+    map.setView([firstLayer.center[0], firstLayer.center[1]], 23);
     
     // Add Google Satellite as default base layer
     baseLayers['Google Satellite'].addTo(map);
